e21e6af09814abd961060122faa1095ee0fc51f7
Anil Jangir <aniljangir212@gmail.com>
2024-07-01 20:45:27 +0530

    added swagger configuration

--- src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
+++ src/main/java/com/polarbookshop/catalog_service/CatalogServiceApplication.java
@@ -1,11 +1,16 @@
 package com.polarbookshop.catalog_service;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 
-@ConfigurationPropertiesScan
 @SpringBootApplication
+@ConfigurationPropertiesScan
+@OpenAPIDefinition(info =
+@Info(title = "Catalog API", version = "${springdoc.version}", description = "Documentation Catalog API v1.0")
+)
 public class CatalogServiceApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/polarbookshop/catalog_service/controller/HomeController.java
+++ src/main/java/com/polarbookshop/catalog_service/controller/HomeController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
+
 @RestController
 public class HomeController {
 
@@ -25,7 +26,7 @@ public class HomeController {
         this.userDataRepository = userDataRepository;
     }
 
-    @GetMapping("/")
+    @GetMapping("/greeting")
     public ResponseEntity<String> getGreeting() {
         return ResponseEntity.ok(polarConfigurationProperties.getGreeting());
     }
